607b31b841abf2efd675d671dd9895c9e844a7fd
Malware3447 <salam0nnrem@gmail.com>
2025-06-18 21:05:30 +0300

    App Init

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,21 @@
 package app
+
+import (
+	"context"
+	"test-task-two/internal/api"
+)
+
+type App struct {
+	router *api.Router
+}
+
+func NewApp(router *api.Router) *App {
+	return &App{router: router}
+}
+
+func (a *App) Init(ctx context.Context) {
+	const op = "app.Init"
+	ctx = context.WithValue(ctx, "app", op)
+
+	a.router.Init(ctx)
+}
